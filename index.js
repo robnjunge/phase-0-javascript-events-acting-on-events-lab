35933a19379f8b072bc5d249420a30db53269405
robertnjunge <mubearobert@gmail.com>
2023-07-11 19:56:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(){
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+
+
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+  document.addEventListener("keydown", function (e) {
+
+    if (e.key === "ArrowLeft") {
+
+        moveDodgerLeft();
+    }
+  });
+
+
+
+
+
+
+
+function moveDodgerRight(){
+
+    const rightNumber = dodger.style.left.replace("px", "");
+    const right =parseInt(rightNumber, 10);
+
+    if (right < 360) {
+
+        dodger.style.left = `${right +1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  });
